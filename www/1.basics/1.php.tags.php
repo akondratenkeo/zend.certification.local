46a469f127cfbad7ee5362a28ac0184f9ac87ae6
Oleksandr Kondratenko <oleksandr.kondratenko@babenkocommerce.com>
2019-08-29 13:36:19 +0300

    Update of 26.08.2019

---
+++ www/1.basics/1.php.tags.php
@@ -0,0 +1,15 @@
+Каким будет результат выполнения следующих выражений?
+===
+
+<?php/*blah*/ echo "blah"?>
+
+<?php /*php followed by space*/ echo "-php followed by space"?>
+
+<?php
+/*php followed by end-of-line*/ echo "-php followed by end-of-line"?>
+
+<?php    /*php followed by tab*/ echo "-php followed by tab"?>
+
+<?php
+
+/*php followed by tab*/ echo "-php followed by tab"
--- www/index.php
+++ www/index.php
@@ -1,13 +1,1 @@
 <?php
-
-function foo()
-{
-    // here you have all global variables
-    extract($GLOBALS);
-
-    echo "inner: $a\n";
-}
-
-$a = "1234";
-
-foo();
