7bd00e31521e40d7e3e5e394dc48320c5bfed5fc
A. Starchenko <alexey.starchenko@babenkocommerce.com>
2019-09-05 03:30:11 +0300

    [Types] Floating point numbers

---
+++ www/sprint2/TypesFloatingpointnumbers/basic_syntaxes.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * LNUM          [0-9]+
+ * DNUM          ([0-9]*[\.]{LNUM}) | ({LNUM}[\.][0-9]*)
+ * EXPONENT_DNUM [+-]?(({LNUM} | {DNUM}) [eE][+-]? {LNUM})
+ */
+$a = 1.234;
+$b = 1.2e3;
+$c = 7E-10;
